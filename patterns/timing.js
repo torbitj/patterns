0760af90bc6fa9d58758fcde927f0439bdf3dba2
JT <jstorbit@gmail.com>
2025-11-12 22:46:48 -0700

    Add conditional for enough food if people is 0

--- patterns/timing.js
+++ patterns/timing.js
@@ -40,5 +40,7 @@ export function isAllEven(numbers) {
  * @returns {boolean} whether there is enough food in the backpack to feed everyone
  */
 export function haveEnoughFood(backpack, people) {
-  // TODO
+  if (people === 0) {
+    return true;
+  }
 }
